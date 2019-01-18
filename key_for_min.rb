f3fe1363b103d539c937f92ac151a95ca0a2b803
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-18 00:26:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     return NIL
   end
   minimum_key,minimum_value = name_hash.first
-  name_hash.sort do |key,value|
+  name_hash.each do |key,value|
     if value < minimum_value
       minimum_key = key
     end
