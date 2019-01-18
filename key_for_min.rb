12a92f09e93ca9784f8986885d429de3930f4a5c
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-18 00:28:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
   minimum_key,minimum_value = nil 
   name_hash.each do |key,value|
     if minimum_value == nil || value < minimum_value
+      minimum_value = validates_each
       minimum_key = key
     end
   end
