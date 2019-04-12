e7fb37d7cba67e1f5d8f18ffd8d4cbd83c75a745
Amy Cohen <amy.cohen@flatironschool.com>
2019-04-12 15:47:00 -0700

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+#SOURCE: Looked at the solution code for this one. I had a similar solution, but it said I was using Key and Value when I had changed the name. I thought I couldn't use the #each method because of this. 
 def key_for_min_value(name_hash)
-
-end
+    low_key = nil
+    low_value = nil
+    name_hash.each do |k, v|
+      if low_value == nil || v < low_value
+        low_value = v
+        low_key = k
+      end
+    end
+    low_key
+  end
